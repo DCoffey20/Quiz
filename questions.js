4d074365e29a8f63a29b04098f828338470223f6
DCOFFEY20 <dcoffey20@gmail.com>
2019-11-10 12:52:24 -0600

    created html and questions js file. Wrote the questions.

---
+++ questions.js
@@ -0,0 +1,27 @@
+let questions = [
+    {
+        title: "Who has the most points in NHL history?",
+        choices: ["Gordie Howe", "Mario Lemieux", "Wayne Gretzky", "Bobby Orr"],
+        answer: "Wayne Gretzky"
+    },
+    {
+        title: "How many 'Gordie Howe Hat Tricks' did Gordie Howe actually have?",
+        choices: ["1", "5", "10", "20"],
+        answer: "1"
+    },
+    {
+        title: "Which NHL franchise has won the most Stanley Cups?",
+        choices: ["Toronto Maple Leafs", "Detroit Red Wings", "Montreal Canadiens", "Boston Bruins"],
+        answer: "Montreal Canadiens"
+    },
+    {
+        title: "Who is the first player to score 60 goals in one season?",
+        choices: ["Wayne Gretzky", "Bobby Hull", "Maurice Richard", "Phil Esposito"],
+        answer: "Phil Esposito"
+    },
+    {
+        title: "What year was the Nashville Predators first season?",
+        choices: ["96-97", "97-98", "98-99", "99-00"],
+        answer: "98-99"
+    },
+];
