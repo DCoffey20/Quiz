950366f185bb80c9510b414e8a747fb5196ea993
DCOFFEY20 <dcoffey20@gmail.com>
2019-11-13 23:00:47 -0600

    updated JS to go through all questions, give you a score at the end, high score button works but high scores not stored yet

--- main.js
+++ main.js
@@ -2,29 +2,100 @@ let quizEl = document.getElementById("quiz");
 let timerEl = document.getElementById("timer");
 let resultsEl = document.getElementById("results");
 let startQuizEl = document.getElementById("startQuiz");
-let startTime = 75;
+let questionsCardEl = document.getElementById("questionsCard");
+let titleEl = document.getElementById("title");
+let choicesEl1 = document.getElementById("choices1");
+let choicesEl2 = document.getElementById("choices2");
+let choicesEl3 = document.getElementById("choices3");
+let choicesEl4 = document.getElementById("choices4");
+let answersEl = document.getElementById("answer");
+let gameOverEl = document.getElementById("gameOver");
+let yourScoreEl = document.getElementById("yourScore");
+let highScoresEl = document.getElementById("highScores");
+let highScoresDisplayEl = document.getElementById("highScoresDisplay");
+let highScoreCloseEl = document.getElementById("highScoreClose");
+let questionsCorrect = 0;
+// let startTime = 75;
+// let i = 0;
 
+choicesEl1.addEventListener("click", userChoice);
+choicesEl2.addEventListener("click", userChoice);
+choicesEl3.addEventListener("click", userChoice);
+choicesEl4.addEventListener("click", userChoice);
+
+questionsCardEl.style.visibility = "hidden";
+gameOverEl.style.visibility = "hidden";
+yourScoreEl.style.visibility = "hidden";
+highScoresDisplayEl.style.visibility = "hidden";
+
+function countdown() {
+    startTime--;
+    timerEl.textContent = startTime;
+    if (startTime === 0) {
+        questionsCardEl.style.visibility = "hidden";
+        gameOverEl.style.visibility = "visible";
+        clearInterval(timer);
+    }
+};
+
+let timer;
 
 startQuizEl.addEventListener("click", function () {
-    let timer = setInterval(function () {
-        startTime--;
-        timerEl.innerHTML = startTime;
-        if (startTime === 0) {
-            clearInterval(timer);
-        }
-    }, 1000);
+    i = 0;
+    startTime = 75;
+    startQuizEl.style.visibility = "hidden";
+    questionsCardEl.style.visibility = "visible";
+    if (i == 0) {
+        showQuestions();
+    };
+    timer = setInterval(countdown, 1000);
+});
+// clicking answers will change through questions.
+function userChoice() {
+    let userAnswer = parseInt(event.target.value);
 
-}
-);
+    console.log(questions[i].answer);
+    if (questions[i].choices[userAnswer] === questions[i].answer) {
+        questionsCorrect++;
+        i++;
+        console.log(questionsCorrect);
+    } else {
+        startTime -= 15;
+        i++;
+    };
 
+    if (i < 5) {
+        console.log(i);
+        showQuestions();
+    } else {
+        let score = startTime;
+        questionsCardEl.style.visibility = "hidden";
+        yourScoreEl.textContent = "Your Score is " + score + "!";
+        yourScoreEl.style.visibility = "visible";
+        clearInterval(timer);
 
-// function () {
-//     let timer = setInterval(function () {
-//         startTime--;
-//         timerEl.innerHTML = startTime;
-//         if (startTime === 0) {
-//             clearInterval(timer);
-//         }
-//     }, 1000);
+    }
+};
 
-// };
+function showQuestions() {
+
+    titleEl.textContent = (questions[i].title);
+    choicesEl1.textContent = (questions[i].choices[0]);
+    choicesEl2.textContent = (questions[i].choices[1]);
+    choicesEl3.textContent = (questions[i].choices[2]);
+    choicesEl4.textContent = (questions[i].choices[3]);
+};
+
+function displayHighScores () {
+highScoresDisplayEl.style.visibility = "visible";
+startQuizEl.style.visibility = "hidden";
+yourScoreEl.style.visibility = "hidden";
+
+};
+
+highScoresEl.addEventListener("click", displayHighScores);
+
+highScoreCloseEl.addEventListener("click", function(){
+    highScoresDisplayEl.style.visibility = "hidden";
+    startQuizEl.style.visibility = "visible";
+})
